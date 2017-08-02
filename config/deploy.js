210f7634ded18c400c43d2e9042d42f4c5df1c4f
Paul Beaudet <inof8or@gmail.com>
2017-08-02 17:19:05 -0400

    Define some off hours for cd to be not active

--- config/deploy.js
+++ config/deploy.js
@@ -1,17 +1,50 @@
 // deploy.js  services that imediately deploys this service when it need to be updated
 var PATH = ' PATH=' + process.env.PATH + ' ';// assuming this is started manually, will help find node/npm, otherwise exact paths are needed
+var CD_HOURS_START = 11;                     // 12 pm Defines hours when deployments can happen
+var CD_HOURS_END   = 22;                     // 11 pm
+
+var getMillis = {
+    toTimeTomorrow: function(hour){                     // millitary hour minus one
+        var currentTime = new Date().getTime();         // current millis from epoch
+        var tomorrowAtX = new Date();                   // create date object for tomorrow
+        tomorrowAtX.setDate(tomorrowAtX.getDate() + 1); // point date to tomorrow
+        tomorrowAtX.setHours(hour, 0, 0, 0);            // set hour to send tomorrow
+        return tomorrowAtX.getTime() - currentTime;     // subtract tomo millis from epoch from current millis from epoch
+    },
+    toOffHours: function(hourStart, hourEnd){
+        var currentDate = new Date();
+        var currentHour = currentDate.getHours();
+        if(currentHour < hourStart || currentHour > hourEnd){
+            return 0;
+        } // it is an off hour there is no millis till off
+        var currentMillis = currentDate.getTime();
+        var offTime = currentDate.setHours(hourEnd, 0, 0, 0);
+        return offTime - currentMillis; // return millis before on time is up
+    }
+};
 
 var jitploy = {
-    io: require('socket.io-client'),                       // to connect to our jitploy intergration server
+    io: require('socket.io-client'),                           // to connect to our jitploy intergration server
+    client: null,
     init: function(server, token, repoName){
-        jitploy.io = jitploy.io(server);                   // jitploy socket server connection initiation
-        jitploy.io.on('connect', function authenticate(){  // connect with orcastrator
-            jitploy.io.emit('authenticate', {
+        jitploy.client = jitploy.io(server);                   // jitploy socket server connection initiation
+        jitploy.client.on('connect', function authenticate(){  // connect with orcastrator
+            jitploy.client.emit('authenticate', {
                 token: token,
                 name: repoName,
-            });                                            // its important lisner know that we are for real
-            jitploy.io.on('deploy', run.deploy);           // respond to deploy events
+            });                                                // its important lisner know that we are for real
+            jitploy.client.on('deploy', run.deploy);           // respond to deploy events
         });
+        var timeToSleep = getMillis.toOffHours(CD_HOURS_START, CD_HOURS_END);
+        setTimeout(function(){
+            jitploy.takeABreak(server, token, repoName);
+        }, timeToSleep);
+    },
+    takeABreak: function(server, token, repoName){
+        jitploy.client.close();                               // stop bothering the server you'll keep it awake
+        setTimeout(function startAgain(){                     // initialize the connection to deployment again tomorrow
+            jitploy.init(server, token, repoName);
+        }, getMillis.toTimeTomorrow(CD_HOURS_START));         // get millis to desired time tomorrow
     }
 };
 
